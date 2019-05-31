c872f34926ae56391772425585bfbe0e32869fb2
Marcel <aurelienmarcel77@gmail.com>
2019-05-31 16:05:13 +0200

    Adding some dependencies

--- src/main/java/com/utbm/lo54/project/controller/CourseController.java
+++ src/main/java/com/utbm/lo54/project/controller/CourseController.java
@@ -4,9 +4,10 @@ import com.utbm.lo54.project.entity.*;
 import com.utbm.lo54.project.service.*;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 public class CourseController {
     @Autowired
     private CourseService courseService;
